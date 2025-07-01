2c3896c869cc52d5e015328147ae98340e2795f4
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-01 13:37:39 +0530

    Add files via upload

---
+++ Lesson2.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+
+int main()
+{
+   int x = 6;
+   int y =5;
+   int sum =x+y ;
+   
+   std::cout<<x <<std::endl;
+   
+std::cout<<y <<std::endl;
+   
+   std::cout<<sum <<std::endl;
+   
+    return 0;
+}
