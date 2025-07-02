851ffe8a763a3c263d66af9c57df116ca1aae2bf
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-02 16:51:14 +0530

    Add files via upload

---
+++ Lesson6.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main()
+{
+    bool student = false ;
+    
+    bool power = true ;
+    
+    std::cout<<student<<std::endl;
+    
+    std::cout<<power<<std::endl;
+    return 0;
+}
