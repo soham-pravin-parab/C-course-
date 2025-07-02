69ae52f5350a1ee6f28e6ce2e57568128e3b1241
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-02 15:22:40 +0530

    Add files via upload

---
+++ Lesson5.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main()
+{
+    char grade ='A';
+    
+    char symbol= '@';
+    
+    std::cout<<grade<<std::endl;
+    std::cout<<symbol<<std::endl;
+
+    return 0;
+}
