8b8974da885d024e02d56a8e11ca86c4b61ab4a1
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-03 15:20:34 +0530

    Add files via upload

---
+++ Lesson8.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    std::string name ="soham";
+    
+    int age = 15;
+    
+    std::cout<<"Hello "<<name<<std::endl;
+    
+    std::cout<<"you are "<<age<< " years old "<<std::endl;
+
+    return 0;
+}
