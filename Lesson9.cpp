31ac248b3f6d5fdcda3b16bcb9dc2286b8cef3d8
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-03 16:00:27 +0530

    Add files via upload

---
+++ Lesson9.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+#include <vector> 
+
+typedef std::string text_t ;
+
+typedef int number_t ;
+
+int main()
+{
+    text_t name= "soham";
+    
+    number_t age = 15;
+    
+    std::cout<<name<<std::endl;
+    
+    std::cout<<age<<std::endl;
+    
+    return 0;
+}
