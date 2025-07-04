90ef86d7c8d45fa2814a40eb6ea8aeaaafbed7b0
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-04 15:10:25 +0530

    Add files via upload

---
+++ Lesson12.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+int main()
+{
+    
+    std:: string name ;
+    
+    int age ;
+    
+    std::cout<<"what is your name ?";
+    
+    std::cin>> name ;
+    
+    std::cout << " what's your age ?"<<std::endl;
+    
+    std::cin>> age ;
+    
+    std::cout<< " Hello "<<name<<std::endl;
+    
+    std::cout<<" Your are "<<age<<" years old"<<std::endl;
+    
+    
+    
+    return 0;
+}
