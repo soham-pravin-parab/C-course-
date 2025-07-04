1dd8ced40a93a23e65c500bb7e5032b8e040c116
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-04 14:20:51 +0530

    Add files via upload

---
+++ Lesson10.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+int main()
+{
+    int students =15; 
+    
+    int year=20;
+    
+    int money =50    ;
+    
+    int weights = 24;
+    
+    students+=5;
+    
+    year--;
+    
+    money*=2;
+    
+    weights/=2;
+    
+    std::cout<<students <<std::endl;
+    
+    std::cout<< year <<std::endl;
+
+    std::cout<< money <<std::endl;
+
+    std::cout<< weights <<std::endl;
+
+    return 0;
+}
