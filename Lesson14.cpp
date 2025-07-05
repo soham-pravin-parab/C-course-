08dd9115caca7af57b38ca9d58e1d71176c6201f
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-05 18:25:41 +0530

    Add files via upload

---
+++ Lesson14.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+int main()
+{
+    double x = 5;
+    
+    double y = 4 ;
+    
+    double A ;
+    
+    double B ;
+    
+    A = std::max(x,y);
+    
+    B = std::min(x,y);
+    
+    std::cout<< A<<'\n';
+    
+    std::cout<< B<<'\n';
+
+    return 0;
+}
