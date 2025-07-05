246b8e3327b44ea1c19a6a340dd7172d1922c312
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-05 18:26:02 +0530

    Add files via upload

---
+++ Lesson15.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+#include <cmath>
+
+int main()
+{
+    double A ;
+    
+    double B ;
+    
+    double C ;
+   
+   A = pow(2,3);
+   
+   B = sqrt(9);
+   
+   C = abs(-6);
+
+    std:: cout<<A<<'\n';
+
+    std:: cout << B<<'\n';
+
+    std:: cout << C<<'\n';
+    
+    
+    return 0;
+}
