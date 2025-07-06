7da1bca7a9b45ac3f1d29bd33a651693628984b8
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-06 15:08:33 +0530

    Add files via upload

---
+++ Lesson16.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+
+# include <cmath>
+
+int main()
+{
+    
+    double x = 3.55;
+    
+    double A;
+    
+    double B;
+    
+    double C;
+    
+    A = round (x);
+    
+    B = ceil (x);
+    
+    C = floor (x);
+    
+    std:: cout << A<<'\n';
+    
+    std:: cout << B<<'\n';
+    
+    std:: cout << C<<'\n';
+
+    return 0;
+}
