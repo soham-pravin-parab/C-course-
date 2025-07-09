06ba07529cb0186ceece984afa2bf8b71e684ebb
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-09 16:51:49 +0530

    Add files via upload

---
+++ Lesson27.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+  std::string grades ;
+    std::cout<<"Enter your grades ";
+    std::cin >> grades ;
+    
+    grades.clear();
+    
+    std::cout<<grades<<" You scored well ";
+
+    return 0;
+}
