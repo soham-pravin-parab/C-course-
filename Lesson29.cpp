0ca311ba312074bb4d90367a0ce71847e799661c
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-10 15:13:30 +0530

    Add files via upload

---
+++ Lesson29.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main()
+{
+    std::string name ;
+    std::cout<<"Enter your name ";
+    std::getline(std::cin,name);
+    
+    name.insert(0,"@");
+    std::cout<<name;
+    
+    
+
+    return 0;
+}
