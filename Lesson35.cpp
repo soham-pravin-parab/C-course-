a427f1d50e55685a0eb7f8fe7595980a9c8ab99e
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-12 14:21:05 +0530

    Add files via upload

---
+++ Lesson35.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+
+int main()
+{
+    for(int i =1;i<=20;i++){
+        if(i==13){
+            break;
+        }
+        std::cout<<i<<'\n';
+        
+    }
+
+    return 0;
+}
