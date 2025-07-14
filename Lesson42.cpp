f07e402f45e6d0c2105dd20e78b63a4bc3ae181b
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-14 15:41:03 +0530

    Add files via upload

---
+++ Lesson42.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+void happyBirthday (std::string name);
+
+int main()
+{
+   std::string name = "Soham";
+   
+   happyBirthday(name);
+   
+    return 0;
+}
+ void happyBirthday(std::string name ){
+     std::cout<<"Happy birthday to you!\n";
+     std::cout<<"Happy birthday to you!\n";
+     std::cout<<"Happy birthday to you!\n";
+     std::cout<<"Happy birthday to you!\n";
+     std::cout<<"Happy birthday to you!\n";
+ }
