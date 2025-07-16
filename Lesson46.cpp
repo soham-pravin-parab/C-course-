d7627bf88b640d4bd38cdcc1fee411c02b2bf744
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-16 14:38:24 +0530

    Add files via upload

---
+++ Lesson46.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main()
+{
+    std::string car[]={"corvette","mustang","camry"};
+    
+    std::cout<<car[0]<<'\n';
+    std::cout<<car[1]<<'\n';
+    
+
+    return 0;
+}
