eed3262c364f98b49f0538d32384bc596ad7180a
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-17 14:29:21 +0530

    Add files via upload

---
+++ Lesson50.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    std::string students [] = {"Ash", "Rock","Johan"};
+    
+    for(int i=0;i<3;i++){
+        std::cout<<students [i]<<'\n';
+    }
+
+    return 0;
+    
+}
+    
