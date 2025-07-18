24cdae42672540628aa8c40752716b97360a671e
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-18 14:56:03 +0530

    Add files via upload

---
+++ Lesson53.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main()
+{
+   
+    
+    char num []  = {'1','2','3','4'};
+    
+    for(char numbers:num){
+        std::cout<<numbers<<'\n' ;
+    }
+    std::cout<<'\n';
+
+    return 0;
+}
