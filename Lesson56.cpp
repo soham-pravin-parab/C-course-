27d054d10447b14ffcd305f450a64be8dacd84b7
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-19 17:48:19 +0530

    Add files via upload

---
+++ Lesson56.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main()
+{
+    std::string foods[100];
+    fill(foods,foods+100,"pizza");
+    
+    for(std::string food:foods){
+        std::cout<<food  <<"  ";
+    }
+    return 0;
+}
