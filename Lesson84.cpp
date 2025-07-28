0cc7df9af68beeb22b4a222540eff856414fc338
soham-pravin-parab <sohamparabmail@gmail.com>
2025-07-28 15:23:03 +0530

    Add files via upload

---
+++ Lesson84.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+   std::vector<int> vec={ 1,2,3,4,5 };
+   
+   
+  for(int val:vec){
+      std::cout<<val<<" ";
+  }
+  
+ 
+   return 0;
+}
