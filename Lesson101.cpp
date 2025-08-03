423e949f91fc08c6a2306b88f8247f273a1f5715
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-03 19:33:48 +0530

    Add files via upload

---
+++ Lesson101.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include  <vector>
+#include  <list>
+using namespace std;
+
+int main()
+{
+    list<int>l;
+    l.emplace_back(1);
+    l.push_back(2);
+    l.push_front(5);
+    
+    
+    for(int val:l){
+        std::cout<<val<<" ";
+    }
+    
+
+    return 0;
+}
