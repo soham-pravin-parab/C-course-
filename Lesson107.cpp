a7aec37dd959edaab1528e6819b4cb2071aa847a
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-05 14:14:26 +0530

    Add files via upload

---
+++ Lesson107.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+  pair<string,int> p={"soham",15};
+  std::cout <<p.first<<" ";
+  std::cout<<p.second;
+
+    return 0;
+}
