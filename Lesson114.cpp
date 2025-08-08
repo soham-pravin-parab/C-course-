9900bd3a1baf48336536162299184b1c239b82a8
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-08 18:18:24 +0530

    Add files via upload

---
+++ Lesson114.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+int main()
+
+{
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    
+    while(!s.empty()){
+        
+    
+    std::cout<<s.top()<<" ";
+    s.pop();
+}
+    return 0;
+}
