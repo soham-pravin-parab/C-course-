8656acdde9d21d877123cfc6074d6caed2354366
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-09 18:12:14 +0530

    Add files via upload

---
+++ Lesson116.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <vector>
+#include <queue> 
+using namespace std;
+int main()
+
+{
+    queue<int>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
+    std::cout<<"top = "<<q.front();
+
+    return 0;
+}
