0e7112f5c0d6ef0ee0f32e1a9c8defa17c1311e7
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-11 11:36:02 +0530

    Add files via upload

---
+++ Lesson123.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string , int>m;
+    
+    m["tv"]=100;
+    m["laptop "]=100;
+    m["headphones "]=50;
+    m.emplace("camera",25);
+    for(auto p:m){
+    
+    std::cout<<p.first<<" "<<p.second<<'\n';
+}
+    std::cout<<"count="<<" "<<m.count("laptop ");
+    return 0;
+}
