b370dd119b5c8ea45ca3f69d4659fe2cab5fc3b4
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-16 15:34:22 +0530

    Add files via upload

---
+++ Lesson135.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+    unordered_set<int>s;
+    s.insert(1); 
+    s.insert(2); 
+    s.insert(3); 
+    s.insert(4); 
+    s.insert(5); 
+    
+    s.insert(1);
+    s.insert(2);
+    s.insert(3); 
+   
+    for(auto val:s){
+        
+    
+    std::cout<<val<<" ";}
+
+    return 0;
+}
