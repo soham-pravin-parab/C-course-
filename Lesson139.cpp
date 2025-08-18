2adb766396da6e0696eb577d0ecf55fe39fe2001
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-18 18:40:21 +0530

    Add files via upload

---
+++ Lesson139.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+   vector <int> vec={1,3,2,5,8};
+    
+   std::sort(vec.begin(),vec.end(),greater<int>());
+    for(int val:vec){
+    std::cout<<val<<" ";
+}
+    return 0;
+}
