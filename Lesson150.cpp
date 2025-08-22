59e909e16ffd92d2638970d1348c1b99421461f5
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-22 11:21:28 +0530

    Add files via upload

---
+++ Lesson150.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main()
+{
+   std::vector <int> vec ={1,2,3,4,5};
+    std::cout<<*(std::min_element (vec.begin(),vec.end()));
+
+    return 0;
+}
