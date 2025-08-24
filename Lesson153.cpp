0de1e018e3e8928d18418452915816f2c183aaf3
soham-pravin-parab <sohamparabmail@gmail.com>
2025-08-24 20:18:58 +0530

    Add files via upload

---
+++ Lesson153.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::unique_ptr<int[]> arr = std::make_unique<int[]>(5);
+
+    for (int i = 0; i < 5; ++i) {
+        arr[i] = (i + 1) * 10;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
