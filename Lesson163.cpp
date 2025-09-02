ab3303358ce9ce838e023b0be00af7222ebb1e1f
soham-pravin-parab <sohamparabmail@gmail.com>
2025-09-02 14:03:19 +0530

    Add files via upload

---
+++ Lesson163.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class person {
+public:
+    int age;
+    string name;
+    
+    person(string name, int age){
+        this->name=name;
+        this->age=age;
+        
+    }
+};
+class student:public person{
+    public :
+        int rollno;
+        
+    student(string name,int age,int rollno):person(name, age ){
+        this->rollno=rollno;
+    }
+    
+    void getinfo(){
+            
+    std::cout<<"name: "<<name<<'\n';
+    
+    std::cout<<"age: "<<age<<'\n';
+    
+    std::cout<<"rollno: "<<rollno<<'\n';
+
+    }
+};
+int main()
+{
+    student s1("soham parab ", 15,123);
+    s1.name="soham parab";
+    s1.age=15;
+    s1.rollno=123;
+    s1.getinfo();
+
+    return 0;
+}
