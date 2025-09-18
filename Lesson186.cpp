43feb3556765d80a6c850dc6828c59c1bef09f97
soham-pravin-parab <sohamparabmail@gmail.com>
2025-09-18 22:43:52 +0530

    Add files via upload

---
+++ Lesson186.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=4;
+    char chArr[]={'a','b','c' ,'d'
+    };
+    for(int i=0;i<n;i++){
+        std::cout<<'\n';
+
+        for(int j=i+1;j>0;j--){
+            std::cout<< chArr[i];
+        }
+    }
+    std::cout<<endl;
+
+    return 0;
+}
