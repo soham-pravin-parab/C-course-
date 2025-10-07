aedeccffc711ff879a2371a5b6c59f2b52858124
soham-pravin-parab <sohamparabmail@gmail.com>
2025-10-07 13:09:50 -0400

    Add files via upload

---
+++ Lesson216.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks[5]={99,100,88,78,65};
+    int size =5;
+    
+    std::cout<<sizeof(marks)/sizeof(int);
+
+    return 0;
+}
