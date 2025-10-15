f4b3e601bb4249fe8c187d6a127d30e97e8cf736
soham-pravin-parab <sohamparabmail@gmail.com>
2025-10-14 20:33:27 -0400

    Add files via upload

---
+++ Lesson228.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int>vec(4,0);
+    std::cout<<vec[0]<<endl;
+    std::cout<<vec[1]<<'\n';
+    std::cout<<vec[2]<<'\n';
+    std::cout<<vec[3]<<'\n';
+
+    return 0;
+}
