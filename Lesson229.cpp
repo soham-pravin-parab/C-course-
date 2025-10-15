b921091ecb9efede13b2a935d7776bc4009ccc04
soham-pravin-parab <sohamparabmail@gmail.com>
2025-10-15 19:31:59 +0530

    Add files via upload

---
+++ Lesson229.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<char>vec={'a','b','c','d','e'};
+   for(char val:vec){
+       cout<<val<<endl;
+   }
+
+    return 0;
+}
