f8346f903ac312087ce32b92d38a8207d319b146
soham-pravin-parab <sohamparabmail@gmail.com>
2025-10-18 22:28:41 +0530

    Add files via upload

---
+++ Lesson235.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int>vec;
+    cout <<"size: "<<vec.size()<<endl;
+    vec.push_back(25);
+    vec.push_back(35);
+    vec.push_back(45);
+    
+    cout<<"after push back size: "<<vec.size()<<endl;
+    vec.pop_back();
+   cout<<vec.at(0)<<endl;
+    return 0;
+}
