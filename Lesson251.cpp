55a6a2125805fee55b69e36381f97c111d36bd09
soham-pravin-parab <sohamparabmail@gmail.com>
2025-11-02 22:30:14 +0530

    Add files via upload

---
+++ Lesson251.cpp
@@ -0,0 +1,48 @@
+
+    
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int>productExceptself(vector<int>& nums){
+    
+    int n=nums.size();
+    vector<int>ans(n,1);
+    vector<int>prefix(n,1);
+    vector<int>suffix(n,1);
+    
+    for(int i=1;i<n;i++){
+        prefix[i]=prefix[i-1]*nums[i-1];
+        
+    }
+    
+    for(int i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1];
+        
+        
+    }
+    for(int i=0;i<n;i++){
+        ans[i]=prefix[i]*suffix[i];
+    }
+
+    return ans;
+    }
+    
+};
+int main()
+{
+    Solution sol;
+    vector<int> nums ={1,2,3,4};
+    vector<int> ans=sol.productExceptself(nums);
+    
+    for(int val:ans){
+        cout<<val<<" ";
+        
+    }
+    cout<<endl;
+    
+    
+    return 0;
+}
