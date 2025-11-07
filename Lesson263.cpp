5b9c903a545d27589391a7719a3fa3d717dca643
soham-pravin-parab <sohamparabmail@gmail.com>
2025-11-07 18:53:43 +0530

    Add files via upload

---
+++ Lesson263.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int a =10;
+    int* ptr =&a;
+    
+    std::cout<<ptr<<endl;
+    ptr++;
+    cout<<ptr<<endl;
+
+    return 0;
+}
