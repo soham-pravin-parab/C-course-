ca4128fe0fcff950be11487a325d7b8679acafd3
soham-pravin-parab <sohamparabmail@gmail.com>
2025-11-17 22:54:47 +0530

    Add files via upload

---
+++ Lesson279.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+bool isPossible(vector <int>&arr,int n,int m,int maxAllowedTime){
+    int painters=1,time=0;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxAllowedTime){
+            return false;
+        }
+        if(time+arr[i]<=maxAllowedTime){
+            time+=arr[i];
+        }
+        else{
+            painters ++;
+            time=arr[i];
+        }
+        if(painters>m){
+            return false;
+        }
+        
+    }
+    return true;
+    
+}
+
+int minTimeToPaint(vector<int>&arr,int n,int m){
+    int sum=0,maxVal=INT_MIN;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        maxVal=max(maxVal,arr[i]);
+        
+    }
+    int st=maxVal,end=sum,ans=-1;
+    
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        
+        if(isPossible(arr,n,m,mid)){
+            ans=mid;
+            end=mid-1;
+        } else{
+            st=mid+1;
+        }
+    }
+    return ans;
+    
+}
+
+int main()
+{
+    vector <int>arr={40,30,10 ,20};
+    int n=4,m=2;
+    
+    
+    std::cout<<minTimeToPaint(arr,n,m);
+
+    return 0;
+}
