ea722ca3db20487c5849d84502ca4a974334718b
soham-pravin-parab <sohamparabmail@gmail.com>
2025-11-23 19:25:49 +0530

    Add files via upload

---
+++ Lesson286.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void printArray (int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void selectionSort(int arr[],int n){
+    
+    for(int i=0;i<n-1;i++){
+        int smallestIndex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]>arr[smallestIndex]){
+            smallestIndex=j;}
+        }
+        
+    
+    swap(arr[i],arr[smallestIndex]);
+    }
+}
+
+int main()
+{
+    int n=5;
+    int arr[]={4,1,5,2,3};
+    selectionSort(arr,n);
+    printArray (arr,n);
+    
+    return 0;
+}
