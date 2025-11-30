68ad630e7534fac1a44b482fb33c3328481d9645
soham-pravin-parab <sohamparabmail@gmail.com>
2025-11-30 22:06:06 +0530

    Add files via upload

---
+++ Lesson293.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout<<"Enter char array "<<endl;
+    cin>>str;
+    std::cout<<"output : "<<str<<endl;
+
+    return 0;
+}
