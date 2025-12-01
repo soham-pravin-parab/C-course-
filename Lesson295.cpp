6cc29bd3f50cd8d29594112e43a94fb705399083
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-01 22:44:55 +0530

    Add files via upload

---
+++ Lesson295.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    
+    std::cout<<"enter char array : ";
+    cin.getline(str,100,'@');
+    cout<<"output : "<<str<<endl;
+
+    return 0;
+}
