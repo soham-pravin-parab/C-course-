b7d2cd5a0c25b08c98c61683476c03dbe7d707ac
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-02 18:05:02 +0530

    Add files via upload

---
+++ Lesson298.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[]="soham Parab ";
+    int len=0;
+    
+    for(int i=0;i<str[i] !='\0';i++){
+        len++;
+    }
+    cout<<"length of the string : "<<len<<endl;
+    return 0;
+}
