9d213cb43a975a4c965c6ea805be83698ade3166
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-03 22:44:40 +0530

    Add files via upload

---
+++ Lesson300.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str="soham parab";
+    
+    
+    std::cout<<str<<endl;
+    
+    str="hello";
+    cout <<str<<endl;
+    return 0;
+}
