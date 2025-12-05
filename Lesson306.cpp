ca0235618adb89dbcfa6de19e2b1005babb2e9fb
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-05 22:43:31 +0530

    Add files via upload

---
+++ Lesson306.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str="soham parab";
+    for(char ch:str){
+        cout<<ch<<" ";
+    }
+    
+    return 0;
+}
