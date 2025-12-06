c6cb26abb6d22e066c08814627e8089593d73759
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-06 22:50:08 +0530

    Add files via upload

---
+++ Lesson309.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        int count0 =0, count1=0,count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }
+        }
+        int idx=0;
+        for(int i=0;i<count0;i++){
+            nums[idx++]=0;
+        }
+        for(int i=0;i<count1;i++){
+            nums[idx++]=1;
+        }
+        for(int i=0;i<count2;i++){
+            nums[idx++]=2;
+        }
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<int> nums={1,0,1,1,2 ,0,2};
+    cout<<"input: ";
+    for(int num:nums){
+        cout << num<<" ";
+        
+    }
+    cout<<endl;
+    sol.sortColors(nums);
+    cout<<"output: ";
+    for(int num:nums){
+        cout<<num<<" ";
+    }
+    std::cout<<endl;
+
+    return 0;
+}
