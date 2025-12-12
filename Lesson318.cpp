4e19d5d76cdc3d1f0be0638b5f65b3b4668c21f9
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-12 23:07:27 +0530

    Add files via upload

---
+++ Lesson318.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int compress(vector<char>&chars){
+        int n=chars.size();
+        int idx=0;
+        
+        for(int i=0;i<n;i++){
+            char ch=chars[i];
+            int count=0;
+            
+            while(i<n && chars[i]==ch){
+                count++;i++;
+            }
+            if(count==1){
+                chars[idx++]=ch;
+            }
+            else{
+                chars[idx++]=ch;
+                string str=to_string(count);
+                for(char dig:str){
+                    chars[idx++]=dig;
+                }
+            }
+            i--;
+        }
+        chars.resize(idx);
+        return idx;
+    }
+};
+int main()
+{
+    Solution sol;
+    vector <char>chars={'a','a','b','b','c','c','c'};
+    int newlen=sol.compress(chars);
+    for(int i=0;i<newlen;i++){
+        cout<<chars[i];
+    }
+    std::cout<<' ';
+
+    return 0;
+}
