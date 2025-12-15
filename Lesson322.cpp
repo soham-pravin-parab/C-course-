b6091d4943222f799ac29d0ff5a56611afbade09
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-15 23:32:21 +0530

    Add files via upload

---
+++ Lesson322.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void printDigits(int n){
+    int count=0;
+    
+    while(n !=0){
+        int digit = n%10;
+        count++;
+        
+        n=n/10;
+    }
+    cout<<count<<endl;
+}
+int main()
+{
+    int n=3568;
+    printDigits(n);
+    
+    return 0;
+}
