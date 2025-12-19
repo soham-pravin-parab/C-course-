b26b1a2b400ec7e62c71c0c05595ca16d5788013
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-19 22:19:06 +0530

    Add files via upload

---
+++ Lesson328.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int gcd(int a ,int b){
+    while(a>0 && b>0){
+        if(a>b){
+            a = a%b;
+        }
+        else{
+            b= b%a;
+        }
+    }
+        if(a==0) return b;
+        
+    return a;
+}
+int main()
+{
+    
+    std::cout<<gcd(6,12);
+
+    return 0;
+}
