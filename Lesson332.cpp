585650c737b773e21b4e307f9c4a06b2c82bcead
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-22 02:10:04 +0530

    Add files via upload

---
+++ Lesson332.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    
+    int matrix [4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    int rows = 4;
+    int cols = 3;
+    
+    cout<<matrix[2][1];
+    
+    return 0;
+}
