309671265e41b8d0960c131d5661d0c160a0e833
soham-pravin-parab <sohamparabmail@gmail.com>
2025-12-26 22:32:48 +0530

    Add files via upload

---
+++ Lesson340.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector <vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+    std::cout<<mat[0][0];
+
+    return 0;
+}
