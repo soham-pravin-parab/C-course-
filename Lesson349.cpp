d9e6f478d7c12745a39bc355c9744c1721b39207
soham-pravin-parab <sohamparabmail@gmail.com>
2026-01-02 21:27:26 +0530

    Add files via upload

---
+++ Lesson349.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int>spiralOrder(vector<vector<int>>& mat){
+        int m =mat.size(),n=mat[0].size();
+        int srow=0,scol=0,erow=m-1,ecol=n-1;
+        vector<int>ans;
+        
+        while(srow<=erow&&scol<=ecol){
+            for(int j=scol;j<=ecol;j++){
+                ans.push_back(mat[srow][j]);
+            }
+            for(int i=srow+1;i<=erow;i++){
+                ans.push_back(mat[i][ecol]);
+            }
+            for(int j=ecol-1;j>=scol;j--){
+                ans.push_back(mat[erow][j]);
+            }
+            for(int i=erow-1;i>=srow+1;i--){
+                ans.push_back(mat[i][scol]);
+            }
+            srow++;scol++;erow--;ecol--;
+        }
+        return ans;
+    }
+    
+};
+int main()
+{
+    Solution sol;
+    vector <vector<int>>mat={{1,2,3},{4,5,6},{7,8,9}};
+    
+   vector<int>result=sol.spiralOrder(mat);
+   for(int num:result ){
+       cout<<num;
+   }
+
+    return 0;
+}
